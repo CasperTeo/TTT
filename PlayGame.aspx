ba3d21b60c35ba427856937df563ff017be82de8
Casper Teo <casper_teo@hotmail.com>
2020-08-11 14:12:38 +0800

    Initial commit

---
+++ PlayGame.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PlayGame.aspx.cs" Inherits="TTT.PlayGame" %>
+
+<!DOCTYPE html>
+
+<style>
+
+    table {
+        width: 200px; min-height: 50px; line-height: 50px; text-align: center; border-collapse: collapse;
+    }
+</style>
+
+<script>
+
+
+</script>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Tic-Tac-Toe</title>
+</head>
+<body>
+    <form id="form1" runat="server" method="get" style="text-align:center">
+    <div>
+        <h1>Welcome to Tic-Tac-Toe</h1>
+        <h2>Turn: <asp:literal id="DisplayTurn" runat="server"></asp:literal></h2>
+        <br />
+        <h3><asp:literal id="result" runat="server"></asp:literal></h3><br />
+    </div>
+        <table align="center" runat="server">
+            <tr>
+                <td>
+                    <asp:Button ID="btn11" runat="server" Text="-" onclick="btn11_OnClick"/>
+                    <asp:literal id="literal11" runat="server"></asp:literal>
+                </td>
+                <td>
+                    <asp:Button ID="btn12" runat="server" Text="-" onclick="btn12_OnClick" />
+                    <asp:literal id="literal12" runat="server"></asp:literal>
+                </td>
+                <td>
+                    <asp:Button ID="btn13" runat="server" Text="-" onclick="btn13_OnClick" />
+                    <asp:literal id="literal13" runat="server"></asp:literal>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btn21" runat="server" Text="-" onclick="btn21_OnClick" />
+                    <asp:literal id="literal21" runat="server"></asp:literal>
+                </td>
+                <td>
+                    <asp:Button ID="btn22" runat="server" Text="-" onclick="btn22_OnClick" />
+                    <asp:literal id="literal22" runat="server"></asp:literal>
+                </td>
+                <td>
+                    <asp:Button ID="btn23" runat="server" Text="-" onclick="btn23_OnClick" />
+                    <asp:literal id="literal23" runat="server"></asp:literal>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btn31" runat="server" Text="-" onclick="btn31_OnClick" />
+                    <asp:literal id="literal31" runat="server"></asp:literal>
+                </td>
+                <td>
+                    <asp:Button ID="btn32" runat="server" Text="-" onclick="btn32_OnClick" />
+                    <asp:literal id="literal32" runat="server"></asp:literal>
+                </td>
+                <td>
+                    <asp:Button ID="btn33" runat="server" Text="-" onclick="btn33_OnClick" />
+                    <asp:literal id="literal33" runat="server"></asp:literal>
+                </td>
+            </tr>
+            <tr>
+                <td colspan ="3">
+                    <br />
+                    <asp:Button ID="resetbtn" runat="server" Text="RESET" onclick="reset_OnClick" />
+                </td>
+            </tr>
+        </table>
+        <br />
+    </form>
+</body>
+</html>
---
+++ PlayGame.aspx.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TTT
+{
+    public partial class PlayGame : System.Web.UI.Page
+    {
+        public static string playerTurn = "1"; //default Player 1 start
+        HttpCookie cookie = HttpContext.Current.Request.Cookies["playerTurn"];
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack){
+                if (cookie == null)
+                {
+                    cookie = new HttpCookie("playerTurn");
+                    cookie.Value = "1";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+            }
+            displayPlayerTurn();
+        }
+
+        protected void btn11_OnClick(object sender, EventArgs e)
+        {
+            playerTurn = GetPlayerTurn();
+            if (literal11.Text.ToString() == "" && result.Text == "")
+            {
+                if (playerTurn == "1")
+                {
+                    literal11.Text = "<img src='./Images/1.png' />";
+
+                    cookie.Value = "2";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                else
+                {
+                    literal11.Text = "<img src='./Images/2.png' />";
+                    cookie.Value = "1";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                btn11.Visible = false;
+                checker();
+            }
+        }
+
+        protected void btn12_OnClick(object sender, EventArgs e)
+        {
+            playerTurn = GetPlayerTurn();
+            if (literal12.Text.ToString() == "" && result.Text == "")
+            {
+                if (playerTurn == "1")
+                {
+                    literal12.Text = "<img src='./Images/1.png' />";
+
+                    cookie.Value = "2";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                else
+                {
+                    literal12.Text = "<img src='./Images/2.png' />";
+                    cookie.Value = "1";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                btn12.Visible = false;
+                checker();
+            }
+        }
+
+        protected void btn13_OnClick(object sender, EventArgs e)
+        {
+            playerTurn = GetPlayerTurn();
+            if (literal13.Text.ToString() == "" && result.Text == "")
+            {
+                if (playerTurn == "1")
+                {
+                    literal13.Text = "<img src='./Images/1.png' />";
+
+                    cookie.Value = "2";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                else
+                {
+                    literal13.Text = "<img src='./Images/2.png' />";
+                    cookie.Value = "1";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                btn13.Visible = false;
+                checker();
+            }
+        }
+
+        protected void btn21_OnClick(object sender, EventArgs e)
+        {
+            playerTurn = GetPlayerTurn();
+            if (literal21.Text.ToString() == "" && result.Text == "")
+            {
+                if (playerTurn == "1")
+                {
+                    literal21.Text = "<img src='./Images/1.png' />";
+
+                    cookie.Value = "2";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                else
+                {
+                    literal21.Text = "<img src='./Images/2.png' />";
+                    cookie.Value = "1";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                btn21.Visible = false;
+                checker();
+            }
+        }
+
+        protected void btn22_OnClick(object sender, EventArgs e)
+        {
+            playerTurn = GetPlayerTurn();
+            if (literal22.Text.ToString() == "" && result.Text == "")
+            {
+                if (playerTurn == "1")
+                {
+                    literal22.Text = "<img src='./Images/1.png' />";
+
+                    cookie.Value = "2";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                else
+                {
+                    literal22.Text = "<img src='./Images/2.png' />";
+                    cookie.Value = "1";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                btn22.Visible = false;
+                checker();
+            }
+        }
+
+        protected void btn23_OnClick(object sender, EventArgs e)
+        {
+            playerTurn = GetPlayerTurn();
+            if (literal23.Text.ToString() == "" && result.Text == "")
+            {
+                if (playerTurn == "1")
+                {
+                    literal23.Text = "<img src='./Images/1.png' />";
+
+                    cookie.Value = "2";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                else
+                {
+                    literal23.Text = "<img src='./Images/2.png' />";
+                    cookie.Value = "1";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                btn23.Visible = false;
+                checker();
+            }
+        }
+
+        protected void btn31_OnClick(object sender, EventArgs e)
+        {
+            playerTurn = GetPlayerTurn();
+            if (literal31.Text.ToString() == "" && result.Text == "")
+            {
+                if (playerTurn == "1")
+                {
+                    literal31.Text = "<img src='./Images/1.png' />";
+
+                    cookie.Value = "2";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                else
+                {
+                    literal31.Text = "<img src='./Images/2.png' />";
+                    cookie.Value = "1";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                btn31.Visible = false;
+                checker();
+            }
+        }
+
+        protected void btn32_OnClick(object sender, EventArgs e)
+        {
+            playerTurn = GetPlayerTurn();
+            if (literal32.Text.ToString() == "" && result.Text == "")
+            {
+                if (playerTurn == "1")
+                {
+                    literal32.Text = "<img src='./Images/1.png' />";
+
+                    cookie.Value = "2";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                else
+                {
+                    literal32.Text = "<img src='./Images/2.png' />";
+                    cookie.Value = "1";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                btn32.Visible = false;
+                checker();
+            }
+        }
+
+        protected void btn33_OnClick(object sender, EventArgs e)
+        {
+            playerTurn = GetPlayerTurn();
+            if (literal33.Text.ToString() == "" && result.Text == "")
+            {
+                if (playerTurn == "1")
+                {
+                    literal33.Text = "<img src='./Images/1.png' />";
+
+                    cookie.Value = "2";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                else
+                {
+                    literal33.Text = "<img src='./Images/2.png' />";
+                    cookie.Value = "1";
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                }
+                btn33.Visible = false;
+                checker();
+            }
+        }
+
+        protected string GetPlayerTurn()
+        {
+            //check cookie
+            if (cookie == null)
+            {
+                cookie = new HttpCookie("playerTurn");
+                playerTurn = "1";
+            }
+            else
+            {
+                playerTurn = cookie.Value;
+            }
+
+            return playerTurn;
+        }
+
+        protected void displayPlayerTurn()
+        {
+            playerTurn = GetPlayerTurn();
+            if (playerTurn == "1")
+                DisplayTurn.Text = "Player <img src='./Images/1.png' />";
+            else
+                DisplayTurn.Text = "Player <img src='./Images/2.png' />";
+        }
+
+        protected bool checker()
+        {
+            if (literal11.Text != "" && literal11.Text == literal12.Text && literal11.Text == literal13.Text)
+            {
+                result.Text = "Player " + literal11.Text + " Won";
+                return true;
+            }
+            else if (literal21.Text != "" && literal21.Text == literal22.Text && literal21.Text == literal23.Text)
+            {
+                result.Text = "Player " + literal21.Text + " Won";
+                return true;
+            }
+            else if (literal31.Text != "" && literal31.Text == literal32.Text && literal31.Text == literal33.Text)
+            {
+                result.Text = "Player " + literal31.Text + " Won";
+                return true;
+            }
+            if (literal11.Text != "" && literal11.Text == literal21.Text && literal11.Text == literal31.Text)
+            {
+                result.Text = "Player " + literal11.Text + " Won";
+                return true;
+            }
+            else if (literal12.Text != "" && literal12.Text == literal22.Text && literal12.Text == literal32.Text)
+            {
+                result.Text = "Player " + literal12.Text + " Won";
+                return true;
+            }
+            else if (literal13.Text != "" && literal13.Text == literal23.Text && literal13.Text == literal33.Text)
+            {
+                result.Text = "Player " + literal13.Text + " Won";
+                return true;
+            }
+            else if (literal11.Text != "" && literal11.Text == literal22.Text && literal11.Text == literal33.Text)
+            {
+                result.Text = "Player " + literal11.Text + " Won";
+                return true;
+            }
+            else if (literal13.Text != "" && literal13.Text == literal22.Text && literal13.Text == literal31.Text)
+            {
+                result.Text = "Player " + literal13.Text + " Won";
+                return true;
+            }
+
+            displayPlayerTurn();
+            return false;
+        }
+
+        protected void reset_OnClick(object sender, EventArgs e)
+        {
+            btn11.Visible = true;
+            btn12.Visible = true;
+            btn13.Visible = true;
+            btn21.Visible = true;
+            btn22.Visible = true;
+            btn23.Visible = true;
+            btn31.Visible = true;
+            btn32.Visible = true;
+            btn33.Visible = true;
+
+            literal11.Text = "";
+            literal12.Text = "";
+            literal13.Text = "";
+            literal21.Text = "";
+            literal22.Text = "";
+            literal23.Text = "";
+            literal31.Text = "";
+            literal32.Text = "";
+            literal33.Text = "";
+
+            result.Text = "";
+
+            cookie = new HttpCookie("playerTurn");
+            cookie.Value = "1";
+            HttpContext.Current.Response.Cookies.Add(cookie);
+            playerTurn = "1";
+            displayPlayerTurn();
+        }
+    }
+}
